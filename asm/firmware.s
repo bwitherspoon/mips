d9ea91312b6562e9227543f27e6301b802a56b2d
Brett Witherspoon <spoonb@cdspooner.com>
2015-06-21 04:14:59 -0500

    sim: improve testbench

--- asm/firmware.s
+++ asm/firmware.s
@@ -1,11 +1,38 @@
+# Arithmetic
 addi $t0, $zero, 0xFFFF
-addi $t1, $zero, 1
-addi $t2, $zero, 5
-addi $t3, $zero, 6
-addi $t4, $zero, 7
-sll  $t1, $t0,   0x10
-nop
-nop
-nop
-nop
+addi $t1, $zero, 0x0001
+addi $t2, $zero, 0x0006
+addi $t3, $zero, 0x0007
+addi $t4, $zero, 0xFF00
+addi $t5, $zero, 0x700A
+add  $t6, $t1,   $t2
+sub  $t7, $t3,   $t2
+# Logical
+and  $t8, $t2,   $t3
+or   $t9, $t2,   $t3
+# Store
+sw   $t0, 0($t0)
+sw   $t1, 0($t0)
 sw   $t2, 0($t0)
+sw   $t3, 0($t0)
+sw   $t4, 0($t0)
+sw   $t5, 0($t0)
+sw   $t6, 0($t0)
+sw   $t7, 0($t0)
+sw   $t8, 0($t0)
+sw   $t9, 0($t0)
+# Shifts
+sll  $t6, $t0,   0x8
+sra  $t7, $t4,   0x8
+srl  $t8, $t4,   0x8
+nop
+nop
+# Store
+sw   $t6, 0($t0)
+sw   $t7, 0($t0)
+sw   $t8, 0($t0)
+# Comparision
+slt  $t9, $t2,   $t3
+# Branch
+beq  $t4, $t4,   0xC
+# Jump
--- sim/cpu_tb.v
+++ sim/cpu_tb.v
@@ -2,7 +2,7 @@
 
 module cpu_tb
 #(
-    parameter MEM_INIT_FILE = "cpu_tb.mem",
+    parameter MEM_INIT_FILE = "cpu_tb.txt",
     parameter VAR_DUMP_FILE = "cpu_tb.vcd"
 );
 
@@ -27,19 +27,116 @@ module cpu_tb
     always #(CLOCK_PERIOD/2) clk <= ~clk;
 
     initial begin
+        // Load memory and dump variables
         $readmemh(MEM_INIT_FILE, cpu.ram.mem, 0, 2**MEM_ADDR_WIDTH-1);
         $dumpfile(VAR_DUMP_FILE);
         $dumpvars(1, clk, rst, gpio);
         $dumpvars(1, cpu.pc, cpu.ir);
+        $dumpvars(0, cpu);
         for (i = 0; i < 2**MEM_ADDR_WIDTH; i = i + 1)
             $dumpvars(1, cpu.ram.mem_[i]);
         for (i = 0; i < 2**REG_ADDR_WIDTH; i = i + 1)
             $dumpvars(1, cpu.regfile.regs_[i]);
 
+        // Reset
         rst = 1;
         #(CLOCK_PERIOD+1) rst = 0;
-        #(32*CLOCK_PERIOD);
 
+        // Arithmetic
+        #(14*CLOCK_PERIOD);
+        if (gpio != 32'hFFFFFFFF) begin
+            $write("Arithmetic test 0 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'hFFFFFFFF, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'd1) begin
+            $write("Arithmetic test 1 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'd1, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'd6) begin
+            $write("Arithmetic test 2 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'd6, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'd7) begin
+            $write("Arithmetic test 3 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'd7, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'hFFFFFF00) begin
+            $write("Arithmetic test 4 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'hFF00, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'h700A) begin
+            $write("Arithmetic test 5 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'h700A, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'd7) begin
+            $write("Arithmetic test 6 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'd7, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'd1) begin
+            $write("Arithmetic test 7 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'd1, gpio);
+            $stop;
+        end
+
+        // Logical
+        #(CLOCK_PERIOD);
+        if (gpio != 32'd6) begin
+            $write("Logical test 0 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'd1, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'd7) begin
+            $write("Logical test 9 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'd7, gpio);
+            $stop;
+        end
+
+        // Shift
+        #(6*CLOCK_PERIOD);
+        if (gpio != 32'hFFFFFF00) begin
+            $write("Shift test 0 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'hFFFFFF00, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'hFFFFFFFF) begin
+            $write("Shift test 1 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'hFFFFFFFF, gpio);
+            $stop;
+        end
+        #(CLOCK_PERIOD);
+        if (gpio != 32'h00FFFFFF) begin
+            $write("Shift test 2 failed - ");
+            $write("Expected: %h, Actual: %h\n", 32'h00FFFFFF, gpio);
+            $stop;
+        end
+
+        // Comparision
+
+        // Branch
+
+        // Jump`
+
+        // Load
+
+        // Store
+
+        #(4*CLOCK_PERIOD);
         $display("All tests succeeded.");
         $finish;
     end
