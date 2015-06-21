d15f70abfcdf9acadf22ad6e11d7b1da21f22824
Brett Witherspoon <spoonb@cdspooner.com>
2015-06-21 01:03:56 -0500

    fetch: initialize IR and cleanup

--- src/fetch.v
+++ src/fetch.v
@@ -7,26 +7,28 @@
 module fetch
 #(
     parameter ADDR_WIDTH = 9,
-    parameter WORD_WIDTH = 32,
+    parameter DATA_WIDTH = 32,
     parameter BOOT_ADDR  = 32'h00000000
 )(
-    input                       clk,
-    input                       rst,
-    input                       jump,
-    input      [WORD_WIDTH-1:0] target,
-    input      [WORD_WIDTH-1:0] data,
-    output     [ADDR_WIDTH-1:0] addr,
-    output reg [WORD_WIDTH-1:0] pc,
-    output reg [WORD_WIDTH-1:0] ir
+    input  wire                  clk,
+    input  wire                  rst,
+    input  wire                  jump,
+    input  wire [DATA_WIDTH-1:0] target,
+    input  wire [DATA_WIDTH-1:0] data,
+    output wire [ADDR_WIDTH-1:0] addr,
+    output reg  [DATA_WIDTH-1:0] pc,
+    output reg  [DATA_WIDTH-1:0] ir
 );
 
     initial pc = BOOT_ADDR;
+    initial ir = {DATA_WIDTH{1'b0}};
 
     assign addr = rst ? BOOT_ADDR[ADDR_WIDTH-1:0] : pc[ADDR_WIDTH-1:0];
 
-    always @(posedge clk) begin
-        ir <= rst ? {WORD_WIDTH{1'b0}} : data;
+    always @(posedge clk)
         pc <= rst ? BOOT_ADDR + 1 : jump ? target : pc + 1;
-    end
+
+    always @(posedge clk)
+        ir <= rst ? {DATA_WIDTH{1'b0}} : data;
 
 endmodule
