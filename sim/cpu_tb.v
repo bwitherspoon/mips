00baf83b88225715e4f40a6a68e8b96e93dbc25b
Brett Witherspoon <spoonb@cdspooner.com>
2015-06-24 15:30:45 -0500

    sim: use task to verify

--- sim/cpu_tb.v
+++ sim/cpu_tb.v
@@ -11,6 +11,18 @@ module cpu_tb
     localparam CPU_DATA_WIDTH = 32;
     localparam CLOCK_PERIOD = 10; // 100 MHz
 
+    task verify (
+        input [63:0] name,
+        input [31:0] expected,
+        input [31:0] actual
+    );
+        if (expected != actual) begin
+            $display("FAIL: %s - %h != %h", name, expected, actual);
+            $stop;
+        end else
+            $display("PASS: %s", name);
+    endtask
+
     integer i;
 
     reg clk = 1;
@@ -42,88 +54,23 @@ module cpu_tb
         #(CLOCK_PERIOD+1) reset = 0;
 
         // Arithmetic
-        #(15*CLOCK_PERIOD);
-        if (gpio != 32'hFFFFFFFF) begin
-            $write("Arithmetic test 0 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'hFFFFFFFF, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'd1) begin
-            $write("Arithmetic test 1 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'd1, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'd6) begin
-            $write("Arithmetic test 2 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'd6, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'd7) begin
-            $write("Arithmetic test 3 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'd7, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'hFFFFFF00) begin
-            $write("Arithmetic test 4 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'hFF00, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'h700A) begin
-            $write("Arithmetic test 5 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'h700A, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'd7) begin
-            $write("Arithmetic test 6 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'd7, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'd1) begin
-            $write("Arithmetic test 7 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'd1, gpio);
-            $stop;
-        end
+        #(15*CLOCK_PERIOD) verify("addi 0", 32'hFFFFFFFF, gpio);
+        #(CLOCK_PERIOD) verify("addi 1", 32'd1, gpio);
+        #(CLOCK_PERIOD) verify("addi 2", 32'd6, gpio);
+        #(CLOCK_PERIOD) verify("addi 3", 32'd7, gpio);
+        #(CLOCK_PERIOD) verify("addi 4", 32'hFFFFFF00, gpio);
+        #(CLOCK_PERIOD) verify("addr 5", 32'h700A, gpio);
+        #(CLOCK_PERIOD) verify("add", 32'd7, gpio);
+        #(CLOCK_PERIOD) verify("sub", 32'd1, gpio);
 
         // Logical
-        #(CLOCK_PERIOD);
-        if (gpio != 32'd6) begin
-            $write("Logical test 0 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'd1, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'd7) begin
-            $write("Logical test 9 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'd7, gpio);
-            $stop;
-        end
+        #(CLOCK_PERIOD) verify("and", 32'd6, gpio);
+        #(CLOCK_PERIOD) verify("or", 32'd7, gpio);
 
         // Shift
-        #(6*CLOCK_PERIOD);
-        if (gpio != 32'hFFFFFF00) begin
-            $write("Shift test 0 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'hFFFFFF00, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'hFFFFFFFF) begin
-            $write("Shift test 1 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'hFFFFFFFF, gpio);
-            $stop;
-        end
-        #(CLOCK_PERIOD);
-        if (gpio != 32'h00FFFFFF) begin
-            $write("Shift test 2 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'h00FFFFFF, gpio);
-            $stop;
-        end
+        #(6*CLOCK_PERIOD) verify("sll", 32'hFFFFFF00, gpio);
+        #(CLOCK_PERIOD) verify("sra", 32'hFFFFFFFF, gpio);
+        #(CLOCK_PERIOD) verify("srl", 32'h00FFFFFF, gpio);
 
         // Comparision
 
@@ -132,25 +79,14 @@ module cpu_tb
         // Jump`
 
         // Load
-        #(10*CLOCK_PERIOD);
-        if (gpio != 32'h00FF0000) begin
-            $write("Load upper immediate test 0 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'h00FF0000, gpio);
-            $stop;
-        end
+        #(10*CLOCK_PERIOD) verify("lui", 32'h00FF0000, gpio);
 
         // Arithemtic
-        #(6*CLOCK_PERIOD);
-        if (gpio != 32'h00FFF00C) begin
-            $write("XOR immediate test 0 failed - ");
-            $write("Expected: %h, Actual: %h\n", 32'h00FF0000, gpio);
-            $stop;
-        end
+        #(6*CLOCK_PERIOD) verify("xori", 32'h00FFF00C, gpio);
 
         // Store
 
-        #(25*CLOCK_PERIOD);
-        $display("PASSED");
+        #(10*CLOCK_PERIOD);
         $finish;
     end
 
